0a02197d9a1ee7269749d4194d7103952c0ff9f8
Edouard Klein <edouardklein@gmail.com>
2015-07-11 22:37:49 +0200

    Fixed bugs in QLearning

--- main.py
+++ main.py
@@ -30,52 +30,64 @@ IMAGES = {1: EARTH,
 def random_terrain():
     """Return a randomized terrain"""
     answer = np.ones((I_MAX+1,J_MAX+1))
-    for i,j in zip(nprand.random_integers(0, I_MAX, 60),
-                   nprand.random_integers(0, J_MAX, 60)):
-        answer[i,j] = nprand.random_integers(2, 4)
-    answer[0,0] = -1 if answer[0,0] == 0 else -answer[0,0]
+    #for i,j in zip(nprand.random_integers(0, I_MAX, 3),
+    #               nprand.random_integers(0, J_MAX, 3)):
+    #    answer[i,j] = nprand.random_integers(2, 4)
+    answer[0,0] = -answer[0,0]
+    answer[0,0] = -3  # DEBUG
     return answer
 
 TERRAIN = random_terrain()
 
 ACTIONS = ["UP", "DOWN", "LEFT", "RIGHT", "PICK"]
 
-def phi(s):
-    """Feature vector on the state space"""
+def phi(s, a):
+    """Feature vector on the state-action space"""
     s = abs(s).reshape(-1)
-    answer = np.zeros(4*9)
-    answer[:9] = (s==1)*1.
-    answer[9:18] = (s==2)*1.
-    answer[18:27] = (s==3)*1.
-    answer[27:36] = (s==4)*1.
+    answer = np.zeros(4*9*len(ACTIONS))
+    start = 4*9*ACTIONS.index(a)
+    answer[start:start+9] = (s==1)*1.
+    answer[start+9:start+18] = (s==2)*1.
+    answer[start+18:start+27] = (s==3)*1.
+    answer[start+27:start+36] = (s==4)*1.
     return answer
 
-Q0 = [np.zeros(4*9) for a in ACTIONS]
-Q = Q0
+omega_0 = np.zeros(4*9*len(ACTIONS))
+omega = omega_0
 
-def q_function(q):
+def q_function(omega):
     """Returns the q function from the weight vectors"""
-    def answer(s):
-        return [np.dot(omega, phi(s)) for omega in q]
+    def answer(s, a):
+        return np.dot(omega, phi(s, a))
     return answer
 
-def random_greedy(q, s):
+def greedy(q, s):
     """Choose an action according to a random choice wheited by the Q-value"""
     # From https://docs.python.org/dev/library/random.html
-    weights = q(s)
-    weights += min(weights) + 1  # All weights > 0, and adding some weight to all options introduces some randomness
-    assert len(weights) == len(ACTIONS)
-    assert all(weights >= 0)
-    cumdist = list(itertools.accumulate(weights))
-    x = random.random() * cumdist[-1]
-    try:
-        return ACTIONS[bisect.bisect(cumdist, x)]
-    except IndexError:
-        print("BBBBUUUUUUUUUUUG")
-        print("x is %f"%x)
-        print("Cumul dist is ")
-        print(cumdist)
-        return random.choice(ACTIONS)
+    print(s)
+    qsa = [q(s, a) for a in ACTIONS]
+    if all([x==0 for x in qsa]):
+        a = random.choice(ACTIONS)
+        print("All zero, radomly choosing "+a)
+        return a
+    for a in ACTIONS:
+        print("q(s, %s) = %f"%(a, q(s,a)))
+    a = ACTIONS[np.argmax(qsa)]
+    print("Choosing "+a)
+    return a
+    #weights += min(weights) + 0.00001  # All weights > 0, and adding some weight to all options introduces some randomness
+    #assert len(weights) == len(ACTIONS)
+    #assert all(weights >= 0)
+    #cumdist = list(itertools.accumulate(weights))
+    #x = random.random() * cumdist[-1]
+    #try:
+    #    return ACTIONS[bisect.bisect(cumdist, x)]
+    #except IndexError:
+    #    print("BBBBUUUUUUUUUUUG")
+    #    print("x is %f"%x)
+    #    print("Cumul dist is ")
+    #    print(cumdist)
+    #    return random.choice(ACTIONS)
 
 def ij2xy(m, i, j):
     "We use matrix-oriented coordinates i,j, but to display we need oriented abc/ord x, y instead"
@@ -136,13 +148,13 @@ def apply_action(m, action):
     m[tuple(robot_loc)] = -m[tuple(robot_loc)]
     return m
 
-def walk(m, q, length):
+def walk(m, q, length, rand=0):
     """Apply the greedy policy with respect to the given q and return the list of
     state, action pairs"""
     answer = [m]
     for i in range(0, length):
         s = robot_state(m)
-        a = random_greedy(q, s)
+        a = greedy(q, s) if np.random.rand() > rand else random.choice(ACTIONS)
         m = apply_action(m, a)
         answer += [a, m]
     return answer
@@ -176,32 +188,44 @@ def sars(ma):
 def Q_learning(Q, sars):
     answer = Q.copy()
     Q_func = q_function(answer)
-    alpha = 0.1  # PARAMETER
+    alpha = 1  # PARAMETER
     gamma = 0.9  # PARAMETER
-    regression_X = [[] for a in ACTIONS]
-    regression_Y = [[] for a in ACTIONS]
-    for s1, a, r, s2 in sars:
-        old = Q_func(s1)[ACTIONS.index(a)]
-        Vs2 = max(Q_func(s2))
-        new = old + alpha*(r + gamma*Vs2 - old)
-        regression_X[ACTIONS.index(a)].append(phi(s1))
-        regression_Y[ACTIONS.index(a)].append(new)
-    #import pdb
-    #pdb.set_trace()
-    for i in range(len(ACTIONS)):
-        if regression_X[i] and regression_Y[i]:
-            answer[i],_,_,_ = np.linalg.lstsq(np.array(regression_X[i]),
-                                        np.array(regression_Y[i]))
+    d = float('inf')
+    q_iter = 0
+    assert not all([r==0 for _,_,r,_ in sars])
+    while d>1. or q_iter<5:
+        old_answer = answer.copy()
+        X = []
+        Y = []
+        for s1, a, r, s2 in sars:
+            old = Q_func(s1, a)
+            Vs2 = max([Q_func(s2,a) for a in ACTIONS])
+            new = old + alpha*(r + gamma*Vs2 - old)
+            X.append(phi(s1, a))
+            Y.append(new)
+        X = np.array(X)
+        Y = np.array(Y)
+        #answer,_,_,_ = np.linalg.lstsq(np.array(X),
+        #                                  np.array(Y))
+        answer = np.dot(np.dot(np.linalg.pinv(np.dot(X.T,X)), X.T), Y)
+        print("NORM of omega %f"%np.linalg.norm(answer))
+        old_q = np.array([Q_func(s,a) for s,a,_,_ in sars])
+        Q_func = q_function(answer)
+        new_q = np.array([Q_func(s,a) for s,a,_,_ in sars])
+        d = np.linalg.norm(old_q-new_q)
+        old_answer = answer.copy()
+        print("Iteration %d, |Q(s,a) - Q'(s,a)| is %f"%(q_iter,d))
+        q_iter+=1
     return answer
 
-def print_q(q):
+def print_omega(omega):
     terrains = ["EARTH", "BUSH", "BERRIES", "SHROOMS"]
     for a in range(len(ACTIONS)):
         print(ACTIONS[a])
         for t in range(len(terrains)):
             print(terrains[t])
-            omega = q[a][9*t:9*t+9]
-            print(omega.reshape(3,3))
+            x = omega[4*9*a + 9*t:4*9*a + 9*t+9]
+            print(x.reshape(3,3))
 
 
 @WINDOW.event
@@ -222,7 +246,7 @@ def on_draw():
 def on_key_press(symbol, modifiers):
     print(symbol)
     global TERRAIN
-    global Q
+    global omega
     if symbol == key.RIGHT:
         TERRAIN = apply_action(TERRAIN, "RIGHT")
     elif symbol == key.LEFT:
@@ -235,14 +259,28 @@ def on_key_press(symbol, modifiers):
         TERRAIN = apply_action(TERRAIN, "PICK")
     elif symbol == key.R:  # Randomize
         TERRAIN = random_terrain()
-    elif symbol == key.W:  # Walk
+    elif symbol == key.T:  # Train
         print('Walking')
-        ma = walk(TERRAIN, q_function(Q), 100)
-        sars_list = sars(ma)
-        Q = Q_learning(Q, sars_list)
+        sars_list = []
+        for i in range(10):
+            ma = walk(TERRAIN, q_function(omega), 10, rand=.5)
+            sars_list += sars(ma)
+        omega = Q_learning(omega, sars_list)
         #pyglet.clock.schedule_once(lambda t: display_traj(ma[::2]), 0)
         TERRAIN = ma[-1]
-        print_q(Q)
+        print_omega(omega)
+    elif symbol == key.S:  # Step
+        print("Stepping")
+        a = greedy(q_function(omega), robot_state(TERRAIN))
+        print(a)
+        TERRAIN = apply_action(TERRAIN, a)
+    ROBOT_WINDOW.dispatch_event('on_draw')
+
+@WINDOW.event
+def on_mouse_press(x, y, button, modifiers):
+    j = x//32
+    i = (I_MAX*32-y)//32 + 1
+    TERRAIN[i,j] = TERRAIN[i,j] + 1 if TERRAIN[i,j] != 4 else 1
     ROBOT_WINDOW.dispatch_event('on_draw')
 
 pyglet.app.run()
