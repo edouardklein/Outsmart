6c2975c63062c59b01c58d68e8377b636d55e822
Denis Baheux <denis@baheux.info>
2015-08-03 01:09:53 +0200

    First functionnal version of save/load functions in the editor

--- level_editor.py
+++ level_editor.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 import outsmart as osmt
 from sys import argv
-import dill
-import pickle
+import dill as pickle
+#import pickle
+import os
 
 def load_state(filename):
-	return pickle.load(open(filename, 'r'))
+	with open("maps/%s"%filename, 'rb') as load_file:
+		s = pickle.load(load_file)
+	return s
 
 def save_state(s, filename):
-	pickle.dump(s, open(filename, 'w'))
+	with open("maps/%s"%filename, 'wb') as save_file:
+		pickle.dump(s, save_file)
 
 def init():
 	osmt.STATE.lab[0, 0] = -1
@@ -16,16 +20,18 @@ def init():
 if __name__ == "__main__":
 
 	#FIXME
-	FILENAME = "maps/mapfile.pickle"
-
+	filename = "current_mapfile.map"
 	init()
 
 	def load_cb():
-		osmt.STATE = load_state(FILENAME)
-	osmt.create_save_button(lambda:save_state(osmt.STATE, FILENAME))
-	#osmt.create_load_button(lambda:osmt.STATE = load_state(FILENAME))
-	osmt.create_load_button(load_cb)	
-	
+		osmt.STATE = load_state(filename)
+
 	if len(argv)>1:
-		osmt.STATE = load_state(argv[1])
+		filename = argv[1]
+		if os.path.isfile("maps/%s"%filename):
+			load_cb()
+
+
+	osmt.create_load_button(load_cb, "Load Map")	
+	osmt.create_save_button(lambda:save_state(osmt.STATE, filename), "Save Map")
 	osmt.pyglet.app.run()
