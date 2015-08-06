d0bceaf28277235e8ec684e4965dbb46f6a2c674
Edouard Klein <edouardklein@gmail.com>
2015-08-06 15:41:22 +0200

    Strip the editor to a bare minimum

--- levels/99level_editor/level_editor.py
+++ levels/99level_editor/level_editor.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
-import outsmart as osmt
-
-from sys import argv
-import os
+import graphics
+import ui
 
 
-if __name__ == "__main__":
-    #FIXME filename, dirname, and directory hierarchy for the lvl componants (maps, script, ...)
-    if len(argv)>1:
-        osmt.STATE.filename = argv[1]
-    try:
-        osmt.load_cb()
-    except FileNotFoundError:
-        pass
-if osmt.STATE.lab is None:
-    s = osmt.State()
-    osmt.STATE.lab = s.lab
-osmt.STATE.active_ui = {k:True for k in osmt.STATE.active_ui}
-osmt.STATE.active_ui["Lab"] = False
-osmt.STATE.active_ui["MainScreen"] = False
-osmt.STATE.level_editor = True
+graphics.STATE.ui.active = ui.ALL_INACTIVE
+graphics.STATE.ui.active.update(ui.EDITOR_ACTIVE)
